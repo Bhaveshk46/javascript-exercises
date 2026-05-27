389a94fde7aa8a7807141749b08b4268065789d3
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-27 23:49:36 +0000

    Use "For loop" and ".isInteger()" for this problem
    Use conditional statement to check is no is positive if yes, then proceed the loop.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let sum = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (a < b && a >= 0 && b >= 0) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+    else if (a > b && a >= 0 && b >= 0) {
+        for (let i = a; i >= b; i--) {
+            sum += i;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return sum;
 };
-
+console.log(sumAll(123, 1))
 // Do not edit below this line
 module.exports = sumAll;
