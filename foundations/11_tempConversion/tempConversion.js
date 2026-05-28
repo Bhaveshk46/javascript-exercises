41b29c9bfd0fa8dd4cd1076997d5052e5cd4b432
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-28 00:46:32 +0000

    Use formula for fahrenheit to celsius and celsius to fahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  //Formula for Fahrenheit to celsius
+  return Number( (5/9 * (num-32) ).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  //Formula for Celsius to Fahrenheit
+  return Number( (num * 9/5+32).toFixed(1))
 };
 
 // Do not edit below this line
