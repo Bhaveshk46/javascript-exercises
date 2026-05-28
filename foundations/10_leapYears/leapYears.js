e4a6fe442bc3cfbf9cea41f6b2c437807e4575c6
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-28 00:19:58 +0000

    Replace the parameter(leapYear) with year to improve readability

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function(leapYear) {
-    if((leapYear%4 ===0 && !(leapYear%100===0))||leapYear%400===0){
+const leapYears = function(year) {
+    if((year%4 ===0 && !(year%100===0))||year%400===0){
         return true;
     }
     else{
